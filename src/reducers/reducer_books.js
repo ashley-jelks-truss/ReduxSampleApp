6c2273f82f2a76413af266254db6f251f042c1f5
Ashley Jelks <ashley@truss.works>
2016-04-11 18:14:39 -0700

    wip BookList component not rendering

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Between Me and the World' },
+    { title: ' Eloquent JavaScript' },
+    { title: ' The Pragmatic Programmer' },
+    { title: 'The Power of Now' }
+  ];
+}
